1c8406986f04128914cb5dde6c9a7ce0a33f5bba
leegwichan <cnddkscndgus@naver.com>
2026-05-25 17:05:52 +0900

    test: 테스트 1차 세팅 진행

---
+++ app-debate/src/test/java/com/debatetracker/debate/persistence/BasePersistenceTest.java
@@ -0,0 +1,11 @@
+package com.debatetracker.debate.persistence;
+
+import com.debatetracker.debate.DatabaseCleaner;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(DatabaseCleaner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public abstract class BasePersistenceTest {
+
+}
---
+++ app-debate/src/test/java/com/debatetracker/debate/repository/BaseRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.debatetracker.debate.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public abstract class BaseRepositoryTest {
+
+}
