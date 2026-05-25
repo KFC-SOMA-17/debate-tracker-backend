b7602aaab272f7b80cd847e04bcad24783034dc8
leegwichan <cnddkscndgus@naver.com>
2026-05-25 17:08:27 +0900

    Revert "test: 테스트 1차 세팅 진행"
    This reverts commit 1c8406986f04128914cb5dde6c9a7ce0a33f5bba.

--- app-debate/src/test/java/com/debatetracker/debate/repository/BaseRepositoryTest.java
+++
@@ -1,8 +0,0 @@
-package com.debatetracker.debate.repository;
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public abstract class BaseRepositoryTest {
-
-}
