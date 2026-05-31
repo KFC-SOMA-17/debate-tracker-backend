056eb4a115e739f74fdfacc2b7ec844d1e29c0e4
leegwichan <cnddkscndgus@naver.com>
2026-05-31 13:34:49 +0900

    feat: Test 환경 - MySQL&Redis 설정

--- app-debate/src/test/java/com/debatetracker/debate/infrastructure/persistence/BaseDomainRepositoryTest.java
+++ app-debate/src/test/java/com/debatetracker/debate/infrastructure/persistence/BaseDomainRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.debatetracker.debate.infrastructure.persistence;
 
 import com.debatetracker.debate.DatabaseCleaner;
+import com.debatetracker.debate.config.TestcontainersConfiguration;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @ExtendWith(DatabaseCleaner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 abstract class BaseDomainRepositoryTest {
