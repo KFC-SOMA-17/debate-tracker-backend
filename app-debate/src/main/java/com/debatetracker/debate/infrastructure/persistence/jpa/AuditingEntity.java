5aaf7e8a259386702d6c6288e55d8cce3055f9f7
Chung-an Lee <44027393+leegwichan@users.noreply.github.com>
2026-06-13 13:31:36 +0900

    [REFACTOR] AuditingEntity 생성자 접근제어자 수정 (#48)
    refactor: AuditingEntity 생성자 접근제어자 수정

--- app-debate/src/main/java/com/debatetracker/debate/infrastructure/persistence/jpa/AuditingEntity.java
+++ app-debate/src/main/java/com/debatetracker/debate/infrastructure/persistence/jpa/AuditingEntity.java
@@ -6,9 +6,11 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @MappedSuperclass
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public abstract class AuditingEntity {
 
     @Column(nullable = false, updatable = false)
